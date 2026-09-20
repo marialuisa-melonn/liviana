358fe226c3c349eb38d963f1bc9e26868deab93b
marialuisa-melonn <marialuisa@melonn.com>
2026-09-20 16:02:16 -0500

    Actualizar sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+/* Cache simple: la app funciona sin internet una vez abierta. */
+const C = "liviana-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => { const cp = r.clone(); caches.open(C).then(c => c.put(e.request, cp)).catch(()=>{}); return r; })
+      .catch(() => caches.match(e.request).then(m => m || caches.match("./index.html")))
+  );
+});
